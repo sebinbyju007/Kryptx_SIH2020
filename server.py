d006ad3acc237b355982f5f98f4f48f737dbe9c0
Royal Benny <45265641+RoyalBenny@users.noreply.github.com>
2020-08-02 10:54:53 +0530

    Server Program Updated

--- server.py
+++ server.py
@@ -5,31 +5,38 @@ import pickle
 import numpy as np
 import struct ## new
 import zlib
-import time
+import pyttsx3
+import main_function_file
+
+
+engine = pyttsx3.init()
+engine.setProperty('rate',100)
+engine.setProperty('voice',engine.getProperty('voices')[1].id)
 
 HOST=''
 PORT=8485
-
+mainFunction = main_function_file.MainFunction()
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 print('Socket created')
-
 
 s.bind((HOST,PORT))
 print('Socket bind complete')
 s.listen(10)
 print('Socket now listening')
-
+engine.say('Started')
+engine.runAndWait()
 conn,addr=s.accept()
 
 data = b""
 payload_size = struct.calcsize(">L")
 print("payload_size: {}".format(payload_size))
-try:
-    while True:
+while True:
+    try:
         while len(data) < payload_size:
             print("Recv: {}".format(len(data)))
+            # if len(data)==0:
+            #     raise Exception('Client connection loss')
             data += conn.recv(4096)
-
         print("Done Recv: {}".format(len(data)))
         packed_msg_size = data[:payload_size]
         data = data[payload_size:]
@@ -41,13 +48,19 @@ try:
         data = data[msg_size:]
 
         frame=pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-        image = cv2.imdecode(frame['image'], cv2.IMREAD_COLOR)
         print('value  is '+str(frame['value']))
-        cv2.imshow('ImageWindow',image)
-        cv2.waitKey(1)
+        val = frame['value']
+        image = cv2.imdecode(frame['image'], cv2.IMREAD_COLOR)
+        cv2.imshow('window',image)
+        # try:
+        output_text = mainFunction.main_function(image,val)
+        print(output_text)
+        engine.say(output_text)
+        engine.runAndWait()
+        # except :
+        #     print('bad image')
         data_new=b'"hai"'
         conn.sendall(data_new)
-	
-
-except:
-    sys.exit()
+    except :
+        print('client connection loss')
+        sys.exit()
