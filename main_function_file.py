04e8d7bb5a66e7e5fe48c4daaec6ed0dd240ab1e
Royal Benny <45265641+RoyalBenny@users.noreply.github.com>
2020-08-03 23:18:29 +0530

    Added face detection codes in Main

--- main_function_file.py
+++ main_function_file.py
@@ -1,19 +1,25 @@
 import cv2
 import yolo_object_detection_file
+import largest_text_detection_file
+# import currency_detection_file
 import full_text_detection_file
 import currency_identifier_new_file
 import sign_board_detection_file
 import medicine_remainder_file
 import medicine_detection_file
+import face_detection_file
 class MainFunction:
     def __init__(self):
         self.yolo_object_detection = yolo_object_detection_file.Yolo_object_detection()
+        self.faceDetection = face_detection_file.FaceDetection()
         # self.q = list()
-        self.fullTextDetection = full_text_detection_file.FullTextDetection()
+        self.largestTextDetection = largest_text_detection_file.LargestTextDetection()
+        # self.currencyClassifier = currency_detection_file.Currency_Classifier()
         self.currencyIdentifier = currency_identifier_new_file.CurrencyIdentifier()
+        self.fullTextDetection = full_text_detection_file.FullTextDetection()
         self.signBoardDetection = sign_board_detection_file.SignBoardDetection()
         self.medicineDetection = medicine_detection_file.MedicineDetection()
-        
+
 
     
 
@@ -22,27 +28,34 @@ class MainFunction:
         obj_arr = []
         pos_arr = []
 
-        
+        if val == 3:
+            cv2.imwrite('image.jpg',img)
+            try:
+                return self.largestTextDetection.detect_largest_text()
+            except :
+                return 'No Text Found or Image not clear'
 
-        if val == 1:
-                cv2.imwrite('image.jpg',img)
-                try:
-                    return self.fullTextDetection.detectText()
-                except :
-                    return 'No text Found or Image Not Clear'
         elif val == 2:
             cv2.imwrite('image.jpg',img)
             try:
                 return self.currencyIdentifier.identify_currency()
             except:
                 return 'No text Found or Image Not Clear'
+
+        elif val == 1:
+                cv2.imwrite('image.jpg',img)
+                try:
+                    return self.fullTextDetection.detectText()
+                except :
+                    return 'No text Found or Image Not Clear'
         elif val== 4:
-            cv2.imwrite('image.jpg',img)
+            cv2.imwrite('image.jpg')
             try:
                 return self.signBoardDetection.classify('image.jpg')
             except:
                 return 'No signal Found or Image Not Clear'
-        elif val == 3:
+
+        elif val == 6:
             cv2.imwrite('image.jpg',img)
             try:
                 return self.medicineDetection.detect_largest_text()+' tablet'
@@ -54,7 +67,8 @@ class MainFunction:
                 return medicine_remainder_file.alarmTrigger()
             except:
                 return 'No output'
-
+        elif val == 7:
+            return 'Good Night Royal , Sweet Dreams, Bye'
 
         else:
 
@@ -92,8 +106,8 @@ class MainFunction:
                 #     continue
                 # else:
                 #     dic[k] = v
-                # if k=='person':
-                #     continue
+                if k=='person':
+                    continue
                 # if len(self.q)>=0:
                 #     self.q.pop(0)
                 # self.q.append(k)
@@ -104,12 +118,27 @@ class MainFunction:
                     output_text+=k+' - '+pos+'. '
                     # output_text=output_text.join(pos+' ')
                     
+            
+
+
+            if 'person' in objects.keys():
+                #  and 'person' not in self.q
+                # print(self.q)
+                cv2.imwrite('image.jpg',frame)
+                person,emmotion = self.faceDetection.face_detection()
+                # if len(person)>=1:
+                #     self.q.append('person')
+
+                for p,e in zip(person,emmotion):
+                    output_text+=p+'-'+e+'. '
+
 
                 
 
 
             return output_text
 
+        
 
 # image2 = cv2.imread('room_ser.jpg')
 # print(mainFunction.main_function(image2,0))
@@ -123,7 +152,7 @@ class MainFunction:
 # print(mainFunction.main_function(image3,0))
 # image = cv2.imread('large_text.jpeg')
 # print(mainFunction.main_function(image,1))
-# image = cv2.imread('image.jpg')
+# image = cv2.imread('modiji.jpg')
 # mainFun = MainFunction()
 # print(mainFun.main_function(image,0))
 
